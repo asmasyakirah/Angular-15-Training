55217d19e3dccfea2fc5f03fe397e14c060b966e
asmasyakirah <asma.syakirahd@gmail.com>
2022-12-08 11:00:40 +0800

    Reactive Form
    Using [formGroup] in Reactive Form.
    
    Note: having validator in TS is much more secure than validator in HTML.

--- src/app/stock/create-stock/create-stock.component.ts
+++ src/app/stock/create-stock/create-stock.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Stock } from 'src/app/model/stock';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-stock',
@@ -13,30 +13,41 @@ export class CreateStockComponent {
   public confirmed = false;
   public exchanges = ['NYSE', 'NASDAQ', 'OTHER'];
 
-  public nameControl = new FormControl();
+  // public nameControl = new FormControl();
+
+  public stockForm: FormGroup = new FormGroup({
+    name: new FormControl(null, Validators.required,),
+    code: new FormControl(null, [Validators.required, Validators.minLength(2)]),
+    price: new FormControl(0, [Validators.required, Validators.min(0)])
+  });
 
   constructor() {
     this.stock =  new Stock('test', 'TS', 0, 0, 'NASDAQ');
   }
 
-  setStockPrice(price: number) 
-  {
-    this.stock.price = price;
-    this.stock.previousPrice = price;
-  }
+  // setStockPrice(price: number) 
+  // {
+  //   this.stock.price = price;
+  //   this.stock.previousPrice = price;
+  // }
 
-  createStock(stockForm:any) 
-  {
-    console.log('Stock Form ', stockForm);
+  // createStock(stockForm:any) 
+  // {
+  //   console.log('Stock Form ', stockForm);
 
-    if (stockForm.valid)
-    {
-      this.stock = stockForm.value.stock;
-      console.log('Stock Form is valid');
-    }
-    else
-    {
-      console.error('Stock Form is invalid state');
-    }
+  //   if (stockForm.valid)
+  //   {
+  //     this.stock = stockForm.value.stock;
+  //     console.log('Stock Form is valid');
+  //   }
+  //   else
+  //   {
+  //     console.error('Stock Form is invalid state');
+  //   }
+  // }
+
+  onSubmit()
+  {
+    console.log('Stock Form value ', this.stockForm.value);
   }
 }
