150a1d4481e1b787ee08e534652fdfa80e45b876
asmasyakirah <asma.syakirahd@gmail.com>
2022-12-08 11:58:54 +0800

    Reactive Form (Form Builder)
    Using [formGroup] and FormBuilder in Reactive Form.
    
    Note: having validator in TS is much more secure than validator in HTML.

--- src/app/stock/create-stock/create-stock.component.ts
+++ src/app/stock/create-stock/create-stock.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Stock } from 'src/app/model/stock';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-stock',
@@ -9,20 +9,20 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class CreateStockComponent {
 
-  public stock: Stock;
-  public confirmed = false;
-  public exchanges = ['NYSE', 'NASDAQ', 'OTHER'];
+  //public stock: Stock;
+  // public confirmed = false;
+  // public exchanges = ['NYSE', 'NASDAQ', 'OTHER'];
 
   // public nameControl = new FormControl();
 
-  public stockForm: FormGroup = new FormGroup({
-    name: new FormControl(null, Validators.required,),
-    code: new FormControl(null, [Validators.required, Validators.minLength(2)]),
-    price: new FormControl(0, [Validators.required, Validators.min(0)])
-  });
-
-  constructor() {
-    this.stock =  new Stock('test', 'TS', 0, 0, 'NASDAQ');
+  public stockForm: FormGroup;
+  constructor (private fb: FormBuilder) 
+  {    
+    this.stockForm = this.fb.group({
+      name: [null, Validators.required],
+      code: [null, [Validators.required, Validators.minLength(2)]],
+      price: [0, [Validators.required, Validators.min(0)]]
+    });
   }
 
   // setStockPrice(price: number) 
